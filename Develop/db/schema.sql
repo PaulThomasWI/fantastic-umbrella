d52458d6aebaca5372231ab808d8fe09c518e88f
PaulThomasWI <87770976+PaulThomasWI@users.noreply.github.com>
2021-10-28 19:30:00 -0500

    sending up code

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,33 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+/*
+USE ecommerce_db;
+
+CREATE TABLE Category (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY 
+    , category_name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE Product (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY 
+    , product_name VARCHAR(128) NOT NULL
+    , price DECIMAL NOT NULL
+    , stock INTEGER NOT NULL
+    , category_id INTEGER
+    , CONSTRAINT fk_category FOREIGN KEY(id) REFERENCES Category(id)
+);
+
+CREATE TABLE Tag (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY 
+    , tag_name VARCHAR(128)
+);
+
+CREATE TABLE ProductTag (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY 
+    , product_id INTEGER
+    , tag_id INTEGER
+    , CONSTRAINT fk_product FOREIGN KEY(id) REFERENCES Product(id)
+    , CONSTRAINT fk_tag FOREIGN KEY(id) REFERENCES Tag(id)        
+);
+*/
